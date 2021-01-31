fd806acebd1678c3e6bf11aad979124fa532285e
helder christ <helder@agenciavinci.com.br>
2021-01-31 19:45:58 -0300

    desktop semipronto

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,16 @@ $( document ).ready(function() {
         slidesToShow: 1,
         dots:true
       });
+      $('.vitrine article > ul').slick({
+        infinite: true,
+        slidesToShow: 5
+      });
+      $('.icons-1 article >ul').slick({
+        infinite: true,
+        slidesToShow: 5
+      });
+      $('.icons-2 article >ul').slick({
+        infinite: true,
+        slidesToShow: 5
+      });
   });
