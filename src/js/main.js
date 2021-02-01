0fa6f0138604497db152d4d4bf2a31ed09af94c2
helder christ <helder@agenciavinci.com.br>
2021-01-31 22:46:19 -0300

    desktop pronto

--- src/js/main.js
+++ src/js/main.js
@@ -13,10 +13,75 @@ $( document ).ready(function() {
       });
       $('.icons-1 article >ul').slick({
         infinite: true,
-        slidesToShow: 5
+        slidesToShow: 5,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 1800,
+            settings: {
+              slidesToShow: 3,
+              centerMode: true,
+              infinite: true,
+              
+            }
+          },
+          {
+            breakpoint: 1220,
+            settings: {
+              slidesToShow: 2,
+              centerMode: true,
+              infinite: true,
+              
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              centerMode: true,
+              infinite: true,
+              
+            }
+          }]
       });
       $('.icons-2 article >ul').slick({
         infinite: true,
-        slidesToShow: 5
+        slidesToShow: 5,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 1800,
+            settings: {
+              slidesToShow: 3,
+              centerMode: true,
+              infinite: true,
+              
+            }
+          },
+          {
+            breakpoint: 1220,
+            settings: {
+              slidesToShow: 2,
+              centerMode: true,
+              infinite: true,
+              
+            }
+          },{
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              centerMode: true,
+              infinite: true,
+              
+            }
+          }]
       });
+      $('.fecha-Modal').on("click",function(){
+        $('.modal-bg').hide();
+        $('.modal').hide();
+      })
   });
